79ff9dfed88fba715251a65fae215828a4dc3ba2
cntruong7701 <cntruong12c5nct@gmail.com>
2022-12-28 02:12:31 +0700

    update 28/12/2022

--- public/wp-content/themes/sage-theme/resources/views/template-parts/page/module-be_lorem.blade.php
+++ public/wp-content/themes/sage-theme/resources/views/template-parts/page/module-be_lorem.blade.php
@@ -1,5 +1,26 @@
 <section class="cus-container wrap">
     <div class="w-4/5 mx-auto center mb-50">
+        @foreach ($data->module['top'] as $item)
+            <div class="flex flex-col width 3xl:px-40 2xl:px-20 margin-col-top">
+                <img src="{!! $item->icon->url !!}" class="w-62 h-74" alt="{!! $item->icon->alt !!}">
+                {{-- <span class="icomoon icon-streamlined text-9xl"></span> --}}
+                {{-- <span class="text-5xl font-bold text-black-200 leading-normal">{!! $item->icon->url !!}</span>
+                <span class="text-4xl 3xl:w-11/12 leading-tight">{!! $item->icon->url !!}</span> --}}
+                {!! $item->des !!}
+            </div>
+            {{-- <div class="flex flex-col width padding-col border--2 border-blue-300">
+                <span class="icomoon icon-icon6 text-9xl"></span>
+                <span class="text-5xl font-bold text-black-200 leading-normal">Scalable,</span>
+                <span class="text-4xl leading-tight">Flexible Solutions</span>
+            </div>
+            <div class="flex flex-col width 3xl:px-40 2xl:px-20 margin-col-bottom">
+                <span class="icomoon icon-icon7 text-9xl"></span>
+                <span class="text-5xl font-bold text-black-200 leading-normal">Backed by</span>
+                <span class="text-4xl 3xl:w-3/4 2xl:w-11/12 leading-tight">Superior Service & Support</span>
+            </div> --}}
+        @endforeach
+    </div>
+    {{-- <div class="w-4/5 mx-auto center mb-50">
         <div class="flex flex-col width 3xl:px-40 2xl:px-20 margin-col-top">
             <span class="icomoon icon-streamlined text-9xl"></span>
             <span class="text-5xl font-bold text-black-200 leading-normal">Streamlined</span>
@@ -15,52 +36,48 @@
             <span class="text-5xl font-bold text-black-200 leading-normal">Backed by</span>
             <span class="text-4xl 3xl:w-3/4 2xl:w-11/12 leading-tight">Superior Service & Support</span>
         </div>
-    </div>
+    </div> --}}
     <div class="relative 2xl:w-4/5 xl:w-full 2xl:ml-100">
         <span class="text-lg font-bold text-brown-400 2xl:p-0 xl:p-56 lg:p-33">THE LATEST</span>
         <div class="flex 2xl:flex-row justify-between md:flex-col 2xl:p-0 xl:px-56 lg:px-33">
 
-            @foreach ($data->module['content'] as $item)
-                <?php
-                $name = get_field('post_title', $item->post_title);
-                echo esc_html($name);
-                ?>
+            @foreach ($data->module['bottom'] as $item)
+                <div class="shadow-2xl 2xl:w-1/2 2xl:m-0 xl:mb-16 lg:mb-17 xl:w-full rounded-b-4xl">
+                    <img class="w-full lazy" src="{!! App::getLogo()['load'] !!}" data-src="{!! $item->image->url !!}" alt="{!! $item->image->alt !!}">
+                    <div class="w-4/5 py-39 mx-auto">
+                        {{-- <h3>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</h3>
+                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p> --}}
+                        {!! $item->content !!}
+                        <button class="text-lg font-bold text-brown-400">
+                            <span>READ MORE</span>
+                            <span class="icomoon icon-chevron-right"></span>
+                        </button>
+                    </div>
+                </div>
+                <div class="2xl:w-1/2 md:w-full 2xl:ml-14 2xl:flex 2xl:flex-col 2xl:justify-between">
+                    <div class="2xl:w-4/5 xl:w-full px-28 py-15 2xl:mb-11 md:mb-16 shadow-2xl rounded-4xl">
+                        {!! $item->content !!}
+                        <button class="text-lg font-bold text-brown-400">
+                            <span>READ MORE</span>
+                            <span class="icomoon icon-chevron-right"></span>
+                        </button>
+                    </div>
+                    <div class="2xl:w-4/5 xl:w-full px-28 py-15 shadow-2xl rounded-4xl">
+                        {!! $item->content !!}
+                        <button class="text-lg font-bold text-brown-400">
+                            <span>READ MORE</span>
+                            <span class="icomoon icon-chevron-right"></span>
+                        </button>
+                    </div>
+                </div>
             @endforeach
-            {{-- <div class="shadow-2xl 2xl:w-1/2 2xl:m-0 xl:mb-16 lg:mb-17 xl:w-full rounded-b-4xl">
-                <img class="w-full" src="{!! App::getBanner()['lorem'] !!}" alt="{!! App::getBanner()['alt'] !!}">
-                <div class="w-4/5 py-39 mx-auto">
-                    <h3>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</h3>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p>
-                    <button class="text-lg font-bold text-brown-400">
-                        <span>READ MORE</span>
-                        <span class="icomoon icon-chevron-right"></span>
-                    </button>
-                </div>
-            </div>
-            <div class="2xl:w-1/2 md:w-full 2xl:ml-14 2xl:flex 2xl:flex-col 2xl:justify-between">
-                <div class="2xl:w-4/5 xl:w-full px-28 py-15 2xl:mb-11 md:mb-16 shadow-2xl rounded-4xl">
-                    <h3>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</h3>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p>
-                    <button class="text-lg font-bold text-brown-400">
-                        <span>READ MORE</span>
-                        <span class="icomoon icon-chevron-right"></span>
-                    </button>
-                </div>
-                <div class="2xl:w-4/5 xl:w-full px-28 py-15 shadow-2xl rounded-4xl">
-                    <h3>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</h3>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p>
-                    <button class="text-lg font-bold text-brown-400">
-                        <span>READ MORE</span>
-                        <span class="icomoon icon-chevron-right"></span>
-                    </button>
-                </div>
-            </div> --}}
         </div>
         <div class="absolute top-0 right-0">
-            <img src="{!! App::getLogo()['load'] !!}" data-src="{!! App::getBanner()['Hide'] !!}" alt="{!! App::getBanner()['alt'] !!}" class="lazy h-200">
+            <img src="{!! App::getLogo()['load'] !!}" data-src="{!! App::getBanner()['Hide'] !!}" alt="{!! App::getBanner()['alt'] !!}"
+                class="lazy h-200">
         </div>
     </div>
 </section>
 
 <?php 
-// var_dump($data->module['content']->post_title);
+// var_dump($data->module['bottom']);
